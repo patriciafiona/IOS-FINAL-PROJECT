7a2afbdb50388c4ae0ff52b218321554e8b586f6
Patricia Fiona <patriciafiona@users.noreply.github.com>
2021-06-30 14:20:18 +0700

    Finsihed Add-Remove Product to SHopping Cart
    Next:
    - Show the list in cart
    - Show total price

--- UnderArmour/UnderArmour/Controller/DetailProductViewController.swift
+++ UnderArmour/UnderArmour/Controller/DetailProductViewController.swift
@@ -8,9 +8,16 @@
 import Foundation
 import UIKit
 
+import iOSDropDown
+
 class DetailProductViewController: UIViewController{
     
     var product: Products?
+    private var sizeVal = ""
+    private var quantityVal = ""
+    
+    @IBOutlet weak var sizeDropdown: DropDown!
+    @IBOutlet weak var quantityDropdown: DropDown!
     
     @IBOutlet weak var horizontalScrollableStackView: UIStackView!
     @IBOutlet weak var horizontalScrollView: UIScrollView!
@@ -26,6 +33,7 @@ class DetailProductViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDropdown()
         
         setDetails()
         
@@ -37,7 +45,54 @@ class DetailProductViewController: UIViewController{
         //check fav button status
         if UserDefaults.standard.object(forKey: "listFavorites") != nil {
             let getListFav = UserDefaults.standard.object(forKey: "listFavorites") as! [[String : Int]]
-            upadteBtnFav(checkProductInListFav(getListFav))
+            upadteBtnFav(checkProductInList(getListFav))
+        }
+        
+        //check cart button status
+        if UserDefaults.standard.object(forKey: "shoppingCart") != nil {
+            let getListCart = UserDefaults.standard.object(forKey: "shoppingCart") as! [[String : String]]
+            updateBtnCart(checkProductInShoppingCart(getListCart))
+        }
+    }
+    
+    private func setDropdown(){
+        //set Dropdown Size
+        var arr = 0...1 //default
+        
+        if product?.tag_name == "Shoes"{
+            if product?.category_name == "Kids"{
+                arr = 30...38
+            }else{
+                arr = 36...45
+            }
+            
+            let SizeArray = arr.map { String($0) }
+            let SizeIndex = arr.map { Int($0) }
+            
+            sizeDropdown.optionIds = SizeIndex
+            sizeDropdown.optionArray = SizeArray
+        }else{
+            //for tops and bottoms
+            let sizeString = ["XS", "S", "M", "L", "XL", "XXL"]
+            sizeDropdown.optionArray = sizeString
+        }
+        
+        sizeDropdown.didSelect{(selectedText , index ,id) in
+            print( "Selected String: \(selectedText) \n index: \(index)")
+            self.sizeVal = selectedText
+        }
+        
+        //set dropdown quantity
+        arr = 1...10
+        let SizeArray = arr.map { String($0) }
+        let SizeIndex = arr.map { Int($0) }
+        
+        quantityDropdown.optionIds = SizeIndex
+        quantityDropdown.optionArray = SizeArray
+        
+        quantityDropdown.didSelect{(selectedText , index ,id) in
+            print( "Selected String: \(selectedText) \n index: \(index)")
+            self.quantityVal = selectedText
         }
     }
     
@@ -55,12 +110,80 @@ class DetailProductViewController: UIViewController{
         }
     }
     
+    private func updateBtnCart(_ status: Bool){
+        if status{
+            //already in cart
+            btnAddToBag.setTitle("Remove from Bag", for: .normal)
+        }else{
+            //not yet in cart
+            btnAddToBag.setTitle("Add to Bag", for: .normal)
+        }
+    }
+    
     private func setImageListener(){
         // create tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(DetailProductViewController.imageTapped(gesture:)))
 
         productImage.addGestureRecognizer(tapGesture)
         productImage.isUserInteractionEnabled = true
+    }
+    
+    @IBAction func addRemoveProductShoppingCart(_ sender: Any) {
+        if sizeVal != "" && quantityVal != "" {
+            //check if already in shopping cart or not
+            let getListCart = UserDefaults.standard.object(forKey: "shoppingCart")
+            
+            if getListCart == nil {
+                var jsonArray: [[String: String]] = [[String: String]]()
+
+                let json = (["product_id": "\(product!.id)", "size": sizeVal, "quantity": quantityVal])
+                jsonArray.append(json)
+                
+                //save in local storage
+                UserDefaults.standard.set(jsonArray, forKey: "shoppingCart")
+                
+                updateBtnCart(true)
+                
+                print(jsonArray)
+            }else{
+                var jsonArray: [[String: String]] = getListCart as! [[String : String]]
+                
+                if checkProductInShoppingCart(jsonArray) == false{
+                    print("Not yet in cart")
+                    
+                    let json = (["product_id": "\(product!.id)", "size": sizeVal, "quantity": quantityVal])
+                    jsonArray.append(json)
+                    
+                    //save in local storage
+                    UserDefaults.standard.set(jsonArray, forKey: "shoppingCart")
+                    
+                    updateBtnCart(true)
+                    
+                    print(jsonArray)
+                }else{
+                    print("Already in cart, remove from the List")
+                    updateBtnCart(false)
+                    
+                    for (index, data) in jsonArray.enumerated(){
+                        if data["product_id"] == "\(product!.id)"{
+                            jsonArray.remove(at: index)
+                            break
+                        }
+                    }
+                    
+                    //update data in local storage
+                    UserDefaults.standard.set(jsonArray, forKey: "shoppingCart")
+                    
+                    print(jsonArray)
+                }
+                
+            }
+        }else{
+            let alert = UIAlertController(title: "Empty Size or Quantity", message: "Please choose Size and Quantity first.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            
+            present(alert, animated: true, completion: nil)
+        }
     }
     
     @IBAction func favoriteBtnOnClick(_ sender: Any) {
@@ -83,7 +206,7 @@ class DetailProductViewController: UIViewController{
         }else{
             var jsonArray: [[String: Int]] = getListFav as! [[String : Int]]
             
-            if checkProductInListFav(jsonArray) == false{
+            if checkProductInList(jsonArray) == false{
                 print("Not yet in fav")
                 
                 let json = (["product_id": product!.id])
@@ -113,13 +236,34 @@ class DetailProductViewController: UIViewController{
         }
     }
     
-    private func checkProductInListFav(_ jsonArray: [[String: Int]] ) -> Bool{
+    private func checkProductInList(_ jsonArray: [[String: Int]] ) -> Bool{
         let currentProduct = (["product_id": product!.id])
         
         var result = false
         
         for product in jsonArray{
             if product == currentProduct{
+                result = true
+                break
+            }else{
+                result = false
+            }
+        }
+        
+        return result
+    }
+    
+    private func checkProductInShoppingCart(_ jsonArray: [[String: String]] ) -> Bool{
+        
+        var result = false
+        
+        for data in jsonArray{
+            if data["product_id"] == "\(product!.id)"{
+                sizeVal = data["size"]!
+                sizeDropdown.text = sizeVal
+                
+                quantityVal = data["quantity"]!
+                quantityDropdown.text = quantityVal
                 result = true
                 break
             }else{
